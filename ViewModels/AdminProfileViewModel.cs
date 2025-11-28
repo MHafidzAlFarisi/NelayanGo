71e642ed1e5676d87ae265e1c35bae770697a849
MHafidzAlFarisi <muhammadhafidzalfarisi@mail.ugm.ac.id>
2025-11-28 22:11:22 +0700

    Add admin raw front end

---
+++ ViewModels/AdminProfileViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NelayanGo.DataServices;
+using NelayanGo.Models;
+using System.ComponentModel;
+
+namespace NelayanGo.ViewModels
+{
+    public class AdminProfileViewModel : INotifyPropertyChanged
+    {
+        private readonly DataServiceNelayan _service = new();
+        private Nelayan? _currentAdmin;
+
+        public Nelayan? CurrentAdmin
+        {
+            get => _currentAdmin;
+            set { _currentAdmin = value; OnPropertyChanged(nameof(CurrentAdmin)); }
+        }
+
+        public AdminProfileViewModel()
+        {
+            CurrentAdmin = _service.GetDataNelayan("ADM-00");
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string prop) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
+
---
+++ Views/AdminHargaIkanWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows;
+using NelayanGo.ViewModels;
+
+namespace NelayanGo
+{
+    public partial class AdminHargaIkanWindow : Window
+    {
+        private AdminHargaIkanViewModel Vm => DataContext as AdminHargaIkanViewModel;
+
+        public AdminHargaIkanWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputHargaButton_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new InputHargaIkanWindow() { Owner = this };
+
+            if (win.ShowDialog() == true)
+            {
+                Vm.AddHargaBaru(win.NamaIkan, win.HargaPerKg);
+            }
+        }
+
+        private void UpdateHargaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Vm.SelectedHarga == null)
+            {
+                MessageBox.Show("Pilih data yang mau diupdate.", "Info",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var sel = Vm.SelectedHarga;
+
+            var win = new UpdateHargaIkanWindow(
+                sel.Id, sel.NamaIkan, sel.HargaPerKilo)
+            {
+                Owner = this
+            };
+
+            if (win.ShowDialog() == true)
+            {
+                Vm.UpdateHarga(win.IdHarga, win.NamaIkanBaru, win.HargaBaruPerKg);
+            }
+        }
+
+        private void DeleteHargaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Vm.SelectedHarga == null)
+            {
+                MessageBox.Show("Pilih data yang mau dihapus.", "Info",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            if (MessageBox.Show(
+                    "Yakin ingin menghapus harga ikan ini?",
+                    "Konfirmasi",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                Vm.DeleteHarga(Vm.SelectedHarga.Id);
+            }
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // sementara kosong, nanti bisa aku bikinin filternya
+        }
+    }
+
+}
+
